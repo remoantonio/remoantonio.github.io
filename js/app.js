e272c953a34640547ad97bdf40c99d491d6c80f2
Remo Gonzalez <remoantonio@gmail.com>
2020-06-18 01:05:48 -0400

    return to div works, sorting needs work

--- js/app.js
+++ js/app.js
@@ -27,44 +27,68 @@ $(() => {
     for (let i = 0; i < heroesArr.length; i++) {
         let img = 'https://hgv-hyperstone.azurewebsites.net/heroes/banner/' + heroesArr[i].name + '.png'
         if (heroesArr[i].primary_attr == 'str') {
-            $('<div>').attr('id', heroesArr[i].id).addClass('heroes').addClass(heroesArr[i].localized_name).appendTo($('#str')).css('background-image', 'url(' + img + ')').attr('draggable', 'true').on('mousedown', pickUp)
+            $('<div>').attr('id', heroesArr[i].id).addClass(heroesArr[i].primary_attr).addClass(heroesArr[i].localized_name).appendTo($('#str')).css('background-image', 'url(' + img + ')').attr('draggable', 'true').on('mousedown', pickUp)
         } else if (heroesArr[i].primary_attr == 'agi') {
-            $('<div>').attr('id', heroesArr[i].id).addClass('heroes').addClass(heroesArr[i].localized_name).appendTo($('#agi')).css('background-image', 'url(' + img + ')').attr('draggable', 'true').on('mousedown', pickUp)
+            $('<div>').attr('id', heroesArr[i].id).addClass(heroesArr[i].primary_attr).addClass(heroesArr[i].localized_name).appendTo($('#agi')).css('background-image', 'url(' + img + ')').attr('draggable', 'true').on('mousedown', pickUp)
         } else if (heroesArr[i].primary_attr == 'int') {
-            $('<div>').attr('id', heroesArr[i].id).addClass('heroes').addClass(heroesArr[i].localized_name).appendTo($('#int')).css('background-image', 'url(' + img + ')').attr('draggable', 'true').on('mousedown', pickUp)
+            $('<div>').attr('id', heroesArr[i].id).addClass(heroesArr[i].primary_attr).addClass(heroesArr[i].localized_name).appendTo($('#int')).css('background-image', 'url(' + img + ')').attr('draggable', 'true').on('mousedown', pickUp)
         }}});
 
     console.log(heroesArr)
 let item;
+// function sort needs work
+    // function sort(location) {
+    //     let holder = $('<div>').attr('id', 'holder')
+    //     $('#' + location[0]).children().detach().appendTo(holder)
+    //     // console.log(location[0])
+    //     for (let i = 0; i < heroesArr.length; i++) {
+    //         if (heroesArr[i].primary_attr == location[0]) {
+    //             $('#' + i).detach().appendTo('#' + location[0])
+    //         }
+    //     }
+    // }
     function pickUp(event) {
         item = $(event.currentTarget)
         return item
     }
     function drop(event) {
-        $(event.currentTarget).text('')
-        $(event.currentTarget).append(item)
-        console.log(item)
-        console.log(item.attr('id'))
+        // introduced logic to return  heroes if replaced by another hero. Still need to order.
+        // console.log($(event.currentTarget).children().length)
+        if ($(event.currentTarget).children().length == 0) {
+            $(event.currentTarget).append(item)
+        } else {
+            // console.log($(event.currentTarget).children().eq(0).attr('class'))
+            // let location = 'str'
+            let location = ($(event.currentTarget).children().eq(0).attr('class')).split(' ')
+            // console.log(location)
+            $(event.currentTarget).children().eq(0).detach().appendTo($('#' + location[0]))
+            // $(event.currentTarget).children().eq(0).detach().appendTo($('#str'))
+            $(event.currentTarget).append(item)
+            // sorting needs work
+            // $('#' + location[0]).children().sort()
+            // console.log($('#' + location[0]).children().sort())
+            // sort(location)
+        }
+        // console.log(item)
+        // console.log(item.attr('id'))
         $.ajax({
             type: "get",
             url: "https://api.stratz.com/api/v1/Hero/" + item.attr('id') + "/matchUp",
             }).then ((matchUp) => {
-                window.item2 = matchUp
-                console.log(matchUp)
+                radPick[($(event.currentTarget).attr('id'))] = matchUp
+                // console.log($(event.currentTarget).attr('id'))
+                // console.log(radPick)
             })
     }
-    $.ajax({
-        type: "get",
-        url: "https://api.stratz.com/api/v1/Hero/1/matchUp",
-        }).then ((matchUp) => {
-            radPick.first = matchUp
-        })
     for (let i = 0; i < 3; i++) {
         $('<div>').attr('id', lanes[i]).appendTo($('#lanes')).addClass('lanes').text(lanes[i])
     }
     for (let i = 0; i < 5; i++) {
-        $('<div>').attr('id', 'pick' + i).appendTo($('#picks')).addClass('picks').text(posTitle[i]).attr('droppable','true').on('dragover', false).on('drop', drop)
+        $('<div>').attr('id', 'pick' + i).appendTo($('#picks')).addClass('picks').attr('droppable','true').on('dragover', false).on('drop', drop)
+        // removed background text from pick locations
+        // $('<div>').attr('id', 'pick' + i).appendTo($('#picks')).addClass('picks').text(posTitle[i]).attr('droppable','true').on('dragover', false).on('drop', drop)
     }
+
     $('#saveDraft').on('click', () => {
         if (localStorage.draftNum == null) {
             localStorage.setItem('draftNum', '0')
