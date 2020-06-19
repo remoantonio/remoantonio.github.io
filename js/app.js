4bef7f021c669c5b98fd8ed4bc732756e21e3867
Remo Gonzalez <remoantonio@gmail.com>
2020-06-19 19:07:42 -0400

    fixed some id issues and fixed heroes returning and numbers going back to normal. also added heroID attr to divs

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 // Creating a null object inside of heroesArr to match hero id with array ID
 let heroesArr = [[null]]
-let radPick ={}
+let radPick = {}
 let pickArr = []
 let lanes = [
     'safelane',
@@ -56,11 +56,16 @@ $(() => {
     for (let i = 0; i < heroesArr.length; i++) {
         let img = 'https://hgv-hyperstone.azurewebsites.net/heroes/banner/' + heroesArr[i].name + '.png'
         if (heroesArr[i].primary_attr == 'str') {
-            $('<div>').attr('id', heroesArr[i].id).addClass(heroesArr[i].primary_attr).addClass(heroesArr[i].localized_name).appendTo($('#str')).css('background-image', 'url(' + img + ')').attr('draggable', 'true').on('mousedown', pickUp)
+            $('<div>').attr('id', i).addClass(heroesArr[i].primary_attr).addClass(heroesArr[i].localized_name).appendTo($('#str')).css('background-image', 'url(' + img + ')').attr('draggable', 'true').on('mousedown', pickUp).attr('heroID', heroesArr[i].id)
         } else if (heroesArr[i].primary_attr == 'agi') {
-            $('<div>').attr('id', heroesArr[i].id).addClass(heroesArr[i].primary_attr).addClass(heroesArr[i].localized_name).appendTo($('#agi')).css('background-image', 'url(' + img + ')').attr('draggable', 'true').on('mousedown', pickUp)
+            $('<div>').attr('id', i).addClass(heroesArr[i].primary_attr).addClass(heroesArr[i].localized_name).appendTo($('#agi')).css('background-image', 'url(' + img + ')').attr('draggable', 'true').on('mousedown', pickUp)
         } else if (heroesArr[i].primary_attr == 'int') {
-            $('<div>').attr('id', heroesArr[i].id).addClass(heroesArr[i].primary_attr).addClass(heroesArr[i].localized_name).appendTo($('#int')).css('background-image', 'url(' + img + ')').attr('draggable', 'true').on('mousedown', pickUp)
+            $('<div>').attr('id', i).addClass(heroesArr[i].primary_attr).addClass(heroesArr[i].localized_name).appendTo($('#int')).css('background-image', 'url(' + img + ')').attr('draggable', 'true').on('mousedown', pickUp)
+        //     $('<div>').attr('id', heroesArr[i].id).addClass(heroesArr[i].primary_attr).addClass(heroesArr[i].localized_name).appendTo($('#str')).css('background-image', 'url(' + img + ')').attr('draggable', 'true').on('mousedown', pickUp)
+        // } else if (heroesArr[i].primary_attr == 'agi') {
+        //     $('<div>').attr('id', heroesArr[i].id).addClass(heroesArr[i].primary_attr).addClass(heroesArr[i].localized_name).appendTo($('#agi')).css('background-image', 'url(' + img + ')').attr('draggable', 'true').on('mousedown', pickUp)
+        // } else if (heroesArr[i].primary_attr == 'int') {
+        //     $('<div>').attr('id', heroesArr[i].id).addClass(heroesArr[i].primary_attr).addClass(heroesArr[i].localized_name).appendTo($('#int')).css('background-image', 'url(' + img + ')').attr('draggable', 'true').on('mousedown', pickUp)
         }}});
 
     console.log(heroesArr)
@@ -68,16 +73,38 @@ let item;
 // synergy function attached to corresponding heroes and sum /////////////////////       FIXED
 // It seems as though brewmaster and omniknight are left out of the synergy calculations from the API
 function synergy() {
-    for (let i = 0; i < radPick[currentPick].advantage[0].with.length; i++) {
-        for (let j = 0; j < synArr.length; j++) {
-            if (synArr[j].heroID == radPick[currentPick].advantage[0].with[i].heroId2) {
-                synArr[j][currentPick] = radPick[currentPick].advantage[0].with[i].synergy
-                synArr[j].sumSyn()
-                synArr[j].setColor()
-                $('#' + radPick[currentPick].advantage[0].with[i].heroId2).text(synArr[j].synTotal).css('background-color', synArr[j].color)
+    for (let i = 0; i < pickArr.length; i++) {
+        if ($('#' + pickArr[i]).children().length == 0) {
+            console.log('yo')
+            radPick[pickArr[i]] = null
+            console.log(radPick)
+            for (let k = 0; k < synArr.length; k++) {
+                synArr[k][pickArr[i]] = 0
             }
         }
     }
+    let nullCounter = 0
+        for (let k = 0; k < 5; k++) {
+            if (radPick[pickArr[k]] != null) {
+                for (let i = 0; i < radPick[[pickArr[k]]].advantage[0].with.length; i++) {
+                    for (let j = 0; j < synArr.length; j++) {
+                        if (synArr[j].heroID == radPick[pickArr[k]].advantage[0].with[i].heroId2) {
+                            synArr[j][pickArr[k]] = radPick[pickArr[k]].advantage[0].with[i].synergy
+                            synArr[j].sumSyn()
+                            synArr[j].setColor()
+                            $('#' + radPick[pickArr[k]].advantage[0].with[i].heroId2).text(synArr[j].synTotal).css('background-color', synArr[j].color)
+                        }
+                    }
+                }
+            } else {
+                nullCounter ++
+                if (nullCounter == 5) {
+                    for (let i = 0; i < synArr.length; i++) {
+                        $('#' + i).text('').css('background-color', 'white')
+                    }
+                }
+            }
+        }
 }
 // sort function ///////////////////////        FIXED
 function sorting(location) {
@@ -91,16 +118,19 @@ function sorting(location) {
         item = $(event.currentTarget)
         return item
     }
+    // Drop at home function////////////////////////////////// Fixed
     function dropHome(event) {
             let location = ($(event.currentTarget).children().eq(0).attr('class')).split(' ')
             let goingTo = (item.attr('class')).split(' ')
             // console.log(location)
-            console.log($(event.target))
+            // console.log($(event.target))
             if ('#' + location[0] == "#" + goingTo[0]) {
                 $(event.currentTarget).append(item)
                 sorting(location)
+                synergy()
             }
     }
+    // Drop in a pick position function/////////////////////////// Fixed
     function drop(event) {
         // introduced logic to return  heroes if replaced by another hero.
         // console.log($(event.currentTarget).children().length)
@@ -118,7 +148,7 @@ function sorting(location) {
         // console.log(item.attr('id'))
         $.ajax({
             type: "get",
-            url: "https://api.stratz.com/api/v1/Hero/" + item.attr('id') + "/matchUp",
+            url: "https://api.stratz.com/api/v1/Hero/" + item.attr('heroID') + "/matchUp",
             }).then ((matchUp) => {
                 radPick[($(event.currentTarget).attr('id'))] = matchUp
                 // console.log($(event.currentTarget).attr('id'))
@@ -134,7 +164,7 @@ function sorting(location) {
     }
     for (let i = 0; i < 5; i++) {
         $('<div>').attr('id', 'pick' + i).appendTo($('#picks')).addClass('picks').attr('droppable','true').on('dragover', false).on('drop', drop)
-        pickArr.push('#pick' + i)
+        pickArr.push('pick' + i)
         // removed background text from pick locations
     }
 
